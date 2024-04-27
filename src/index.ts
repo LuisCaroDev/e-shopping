195f8a8eb7b0e97f95181c240873b12456e17f78
luis angel caro galoc <lucaro@aeromexico.com>
2024-04-26 21:00:28 -0500

    fix: renamed entry point

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import express from 'express';
 import path from 'path';
 
-const app = express();
-app.use(express.static(path.join(__dirname, '..', 'public')));
+export function main() {
+  const app = express();
+  app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-});
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+  });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor iniciado en el puerto ${PORT}`);
-});
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Servidor iniciado en el puerto ${PORT}`);
+  });
+}
